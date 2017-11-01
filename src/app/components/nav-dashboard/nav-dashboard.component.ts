fa7179f0f726228435f48119f20c58c42994b7b4
Mark <markeasting@gmail.com>
2017-11-02 00:17:14 +0100

    Added dashboard, navigation and styling

--- src/app/components/nav-dashboard/nav-dashboard.component.ts
+++ src/app/components/nav-dashboard/nav-dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-dashboard',
   templateUrl: './nav-dashboard.component.html',
-  styleUrls: ['./nav-dashboard.component.css']
+  styleUrls: ['./nav-dashboard.component.scss']
 })
 export class NavDashboardComponent implements OnInit {
 
